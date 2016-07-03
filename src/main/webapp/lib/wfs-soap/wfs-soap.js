c4377001fc8d7116457dcb75c4a00c46f1506638
wushengcq <wushengcq@gmail.com>
2016-07-03 05:28:13 -0700

    enable soap1.1 soap1.2 and soap/http message binding, created three service endpoints

--- src/main/webapp/lib/wfs-soap/wfs-soap.js
+++ src/main/webapp/lib/wfs-soap/wfs-soap.js
@@ -9,7 +9,7 @@ function startup() {
 function initSendButton() {
     $("#sendButton").click(function(e){
     	var soapMessage = $("#soapMessage").val();
-		var endpoint = "http://localhost:8080/services/ows/wfs?";
+		var endpoint = "http://localhost:8080/services/ows/wfs/soap12";
 		var protocol = $("#sendButton").attr("protocol");
 		var header = {'Accept':'text/plain, */*; q=0.01'};
         sendSoap(endpoint, soapMessage, header);
@@ -26,6 +26,7 @@ function sendSoap(endpoint, soapMessage, header) {
 		data: soapMessage,
 		processData: false,  
 		contentType: "application/soap+xml; charset=utf-8",
+		//contentType: "application/xml; charset=utf-8",
 		headers: header,
 		success: OnSuccess, 
 		error: OnError
